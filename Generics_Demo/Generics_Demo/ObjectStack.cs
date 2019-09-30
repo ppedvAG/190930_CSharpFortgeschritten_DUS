9336081342e57a35ee85e19d9735f4f7542a39b6
Michael Zöhling <michael.zoehling@outlook.com>
2019-09-30 12:17:34 +0200

    GenericStack erstellt

---
+++ Generics_Demo/Generics_Demo/ObjectStack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Demo
+{
+    public class ObjectStack
+    {
+        public ObjectStack() : this(4) { } // .NET Framework ist 4 -> Default
+        public ObjectStack(int capacity)
+        {
+            data = new object[capacity];
+            index = 0;
+        }
+
+        private object[] data;
+        private int index;
+
+
+        public void Push(object item)
+        {
+            // Wenn voll: vergrößern:
+            if(data.Length == index)
+            {
+                object[] newData = new object[data.Length * 2];
+                Array.Copy(data, newData,data.Length);
+                data = newData; // Das alte Array wird "irgendwann" vom GC eingesammelt
+            }
+
+            data[index] = item;
+            index++;
+        }
+
+        public object Pop()
+        {
+            if (index == 0)
+                throw new InvalidOperationException("Stack ist bereits leer");
+
+            index--;
+            return data[index];
+        }
+
+
+    }
+}
--- Generics_Demo/Generics_Demo/Program.cs
+++ Generics_Demo/Generics_Demo/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +9,36 @@ namespace Generics_Demo
     {
         static void Main(string[] args)
         {
+            // Problemstellung:
+            //object[] data = new object[5];
+            //data[0] = "12";
+            //data[1] = "abcde";
+            //data[2] = new StringBuilder();
+
+            //int zahl1 = (int) data[0];
+
+            // Eigene "Liste" erfinden
+
+            ObjectStack os = new ObjectStack();
+
+            os.Push(12);
+            os.Push(44);
+            os.Push("Hallo Welt");
+            os.Push(12312312);
+
+            os.Push(8765);
+
+            Console.WriteLine(os.Pop());
+            Console.WriteLine(os.Pop());
+            Console.WriteLine(os.Pop());
+            Console.WriteLine(os.Pop());
+            Console.WriteLine(os.Pop());
+
+            // Console.WriteLine(os.Pop()); // Exception wenn der Stack leer ist
+
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
         }
     }
 }
