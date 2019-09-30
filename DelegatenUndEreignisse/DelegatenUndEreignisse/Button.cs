28d43ac10427daf5725e31aa6083b04059e90579
Michael Zöhling <michael.zoehling@outlook.com>
2019-09-30 16:37:28 +0200

    Variante ohne Event

---
+++ DelegatenUndEreignisse/DelegatenUndEreignisse/Button.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatenUndEreignisse
+{
+    class Button
+    {
+        public Action KlickLogik; // hier kann ich Speichern(); Abbrechen();
+
+        public void MachDenKlick()
+        {
+            // ich will das hier was passiert, weiß aber nicht was
+            // ich brauche die möglichkeit, dass mir wer anderer sagen kann, was ich hier machen soll
+
+            if (KlickLogik != null) // Exception wenn "sich keiner dafür interessiert"
+                KlickLogik.Invoke(); // die hinterlegte logik (meinbutton_click in der Klasse Program.cs)
+        }
+    }
+
+
+    class Person
+    {
+        // Feld
+        private decimal Kontostand; // private -> keiner darf von draussen drauf zugreifen
+
+        // Property
+        public string Name { get; set; }
+    }
+}
--- DelegatenUndEreignisse/DelegatenUndEreignisse/Program.cs
+++ DelegatenUndEreignisse/DelegatenUndEreignisse/Program.cs
@@ -8,15 +8,117 @@ namespace DelegatenUndEreignisse
 {
     class Program
     {
+        // 1)
+
+        // .NET 1.0
+        // Signatur: [public/private...] delegate [rückgabetyp] [Name] ([parameter])
+        public delegate void MeinErsterDelegat();
+        public delegate void MeinZweiterDelegat(int x);
+        // mit ergebnis:
+        public delegate int Rechenart(int z1, int z2);
+        // -> Delegate für ALLES was "void" ist und "keine Parameter" hat
+
         static void Main(string[] args)
         {
+            #region Erklärung: Delegate
+            // Direkter Aufruf:
+            // A();
+            // B();
+            // C(12);
 
+            // Aufruf per Variable (delegate):
+            // 1) Delegate-Type
+            // 2) Delegate-Instanz
+            // 3) Auf der Instanz die ".Invoke()" - Methode aufrufen
 
+            // 2)
+            // MeinErsterDelegat del = new MeinErsterDelegat(B); // Ohne Klammern für A !!!!
 
+            // 3)
+            // del.Invoke(); 
+            #endregion
 
+            #region Abkürzungen
+            // 2) Kürzere Schreibweise
+            // Anstelle von .... = new MeinErsterDelegat(B);
+            // MeinErsterDelegat del = B;
+
+            // 3) Kürzerer Aufruf
+            // Anstelle von ... del.Invoke();
+            // del(); 
+            #endregion
+
+            #region Delegat mit Parameter
+            //MeinZweiterDelegat del = C;
+
+            //del.Invoke(1234);
+            //del(999); 
+            #endregion
+
+            #region Verketten von Delegaten
+            // += und -=
+            //MeinErsterDelegat del = A;
+            //del += B; // Direkt nach A auch B machen
+
+            //del(); 
+            #endregion
+
+            #region Action<T> und Func<T>
+            //// Action<T> für alles, was VOID ist (maximal 16 Parameter)
+            //Action meineAction = A;
+            //Action<int> meineC_Action = C;
+
+            //// Func<T> für alles mit Rückgabewert (max 16 Parameter)
+            //Func<int, int, int> rechner = Summe;
+            //// Das letzte "T" steht für die Ausgabe  
+            #endregion
+
+            // Anwendungsfall
+
+            Button b1 = new Button();
+
+            // Das schreibt WindowsForms für euch beim Doppelklick!!!!
+            b1.KlickLogik = Speichern; // Bei jedem Klick, mach diese Methode !!!
+
+            b1.MachDenKlick();
+            b1.MachDenKlick();
+            b1.MachDenKlick();
+            b1.MachDenKlick();
+            b1.MachDenKlick();
+
+            // Event
+            // Events sind sozusagen das "Property" für Delegaten
 
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
+        }
+
+        public static void Speichern()
+        {
+            Console.WriteLine("Ich speichere");
+        }
+
+        public static void Meinbutton_Click()
+        {
+            Console.Beep();
+            Console.WriteLine("*click*");
+        }
+
+        static int Summe(int z1, int z2)
+        {
+            return z1 + z2;
+        }
+        static void A()
+        {
+            Console.WriteLine("A");
+        }
+        static void B()
+        {
+            Console.WriteLine("B");
+        }
+        static void C(int zahl)
+        {
+            Console.WriteLine($"C{zahl}");
         }
     }
 }
