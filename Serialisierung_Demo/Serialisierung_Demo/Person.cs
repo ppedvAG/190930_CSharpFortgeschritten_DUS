2c4017d718eae5ab179afc5be3d9e5bb249e92db
Michael Zöhling <michael.zoehling@outlook.com>
2019-10-01 16:15:50 +0200

    Variante: JSON

--- Serialisierung_Demo/Serialisierung_Demo/Person.cs
+++ Serialisierung_Demo/Serialisierung_Demo/Person.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Serialisierung_Demo
 {
-    [Serializable] // Benötigt für BinaryFormatter !
+    // [Serializable] // Benötigt für BinaryFormatter !
     public class Person
     {
+        [XmlAttribute]
         public string Vorname { get; set; }
+        [XmlAttribute]
         public string Nachname { get; set; }
         public byte Alter { get; set; }
         public decimal Kontostand { get; set; }
--- Serialisierung_Demo/Serialisierung_Demo/Program.cs
+++ Serialisierung_Demo/Serialisierung_Demo/Program.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Serialisierung_Demo
 {
@@ -20,17 +22,48 @@ namespace Serialisierung_Demo
                 Kontostand = 100
             };
 
-            // Binär:
-            BinaryFormatter formatter = new BinaryFormatter();
-            FileStream stream = new FileStream("Person.bin", FileMode.Create);
-            formatter.Serialize(stream, p1);
-            stream.Close();
+            #region Binär
+            //BinaryFormatter formatter = new BinaryFormatter();
+            //FileStream stream = new FileStream("Person.bin", FileMode.Create);
+            //formatter.Serialize(stream, p1);
+            //stream.Close();
+
+            //// Deserialisieren:
+            //stream = new FileStream("Person.bin", FileMode.Open);
+            ////object geladenePerson = formatter.Deserialize(stream);
+            //Person geladenePerson = (Person)formatter.Deserialize(stream);
+            //stream.Close();
+
+            //Console.WriteLine(geladenePerson.Vorname);
+            //Console.WriteLine(geladenePerson.Nachname); 
+            #endregion
+
+            #region XML
+            //// Limitierungen: Klasse muss Public sein, unterstützte Member ebenfalls !
+
+            //XmlSerializer formatter = new XmlSerializer(typeof(Person));
+            //FileStream stream = new FileStream("Person.xml", FileMode.Create);
+            //formatter.Serialize(stream, p1);
+            //stream.Close();
+
+            //// Deserialisieren
+            //stream = new FileStream("Person.xml", FileMode.Open);
+            //Person geladenePerson = (Person)formatter.Deserialize(stream);
+            //stream.Close();
+
+            //Console.WriteLine(geladenePerson.Vorname);
+            //Console.WriteLine(geladenePerson.Nachname); 
+            #endregion
+
+            // JSON
+            // -> NuGet: Newtonsoft.JSON
+
+            string json = JsonConvert.SerializeObject(p1);
+
+            Console.WriteLine(json);
 
             // Deserialisieren:
-            stream = new FileStream("Person.bin", FileMode.Open);
-            //object geladenePerson = formatter.Deserialize(stream);
-            Person geladenePerson = (Person)formatter.Deserialize(stream);
-            stream.Close();
+            Person geladenePerson = JsonConvert.DeserializeObject<Person>(json);
 
             Console.WriteLine(geladenePerson.Vorname);
             Console.WriteLine(geladenePerson.Nachname);
