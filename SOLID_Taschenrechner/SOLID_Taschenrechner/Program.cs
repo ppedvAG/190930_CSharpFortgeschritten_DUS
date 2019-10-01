2fb47da918e9b6176b17875d15ea8bdca2bc6dd7
Michael Zöhling <michael.zoehling@outlook.com>
2019-10-01 11:19:01 +0200

    Variante ohne SOLID

--- SOLID_Taschenrechner/SOLID_Taschenrechner/Program.cs
+++ SOLID_Taschenrechner/SOLID_Taschenrechner/Program.cs
@@ -10,6 +10,25 @@ namespace SOLID_Taschenrechner
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Bitte geben Sie die Formel ein:");
+            string eingabe = Console.ReadLine(); // "2 + 2"
+
+            string[] teile = eingabe.Split();
+            int zahl1 = Convert.ToInt32(teile[0]);
+            int zahl2 = Convert.ToInt32(teile[2]);
+            string rechenoperator = teile[1];
+
+            int ergebnis = 0;
+            if (rechenoperator == "+")
+                ergebnis = zahl1 + zahl2;
+            else if (rechenoperator == "-")
+                ergebnis = zahl1 - zahl2;
+            // ....
+
+            Console.WriteLine($"Das Ergebnis ist {ergebnis}");
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
         }
     }
 }
