fe35ce6a8538d9ee04542ab94930fd25cf9a9e55
Michael Zöhling <michael.zoehling@outlook.com>
2019-10-01 16:02:12 +0200

    Variante: Binär

--- Serialisierung_Demo/Serialisierung_Demo/Program.cs
+++ Serialisierung_Demo/Serialisierung_Demo/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,31 @@ namespace Serialisierung_Demo
     {
         static void Main(string[] args)
         {
+            Person p1 = new Person
+            {
+                Vorname = "Tom",
+                Nachname = "Ate",
+                Alter = 10,
+                Kontostand = 100
+            };
+
+            // Binär:
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream("Person.bin", FileMode.Create);
+            formatter.Serialize(stream, p1);
+            stream.Close();
+
+            // Deserialisieren:
+            stream = new FileStream("Person.bin", FileMode.Open);
+            //object geladenePerson = formatter.Deserialize(stream);
+            Person geladenePerson = (Person)formatter.Deserialize(stream);
+            stream.Close();
+
+            Console.WriteLine(geladenePerson.Vorname);
+            Console.WriteLine(geladenePerson.Nachname);
+
+            Console.WriteLine("---Anfang---");
+            Console.ReadKey();
         }
     }
 }
