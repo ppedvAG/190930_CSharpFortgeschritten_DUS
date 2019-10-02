3c1c8bc682f7cacaff4ad4e53397d5ab8d8cd640
Michael Zöhling <michael.zoehling@outlook.com>
2019-10-02 09:49:55 +0200

    Grundlagen für Threads

--- Thread_Demo/Thread_Demo/Program.cs
+++ Thread_Demo/Thread_Demo/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Thread_Demo
@@ -10,9 +11,48 @@ namespace Thread_Demo
     {
         static void Main(string[] args)
         {
+            #region Grundlagen Threads
+            //// Action a = new Action(MachEtwasInEinemThread);
+            //// Action b = MachEtwasInEinemThread; 
+
+
+            //Thread t1 = new Thread(MachEtwasInEinemThread);
+            ////  Thread t1 = new Thread(new ParameterizedThreadStart(MachEtwasInEinemThread));
+
+            //Thread t2 = new Thread(MachEtwasInEinemThread2);
+
+            //t1.Start();
+            //t2.Start();
+
+            ////Thread.Sleep(5000);
+            //// Nach 5 sek: Thread beenden
+            ////t1.Abort();
+            ////t2.Abort();
+
+            //// Auf einen Thread warten:
+            //t1.Join();
+            //t2.Join();
+            #endregion
 
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
+        }
+
+        static void MachEtwasInEinemThread()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Thread.Sleep(10);
+                Console.Write("*");
+            }
+        }
+        static void MachEtwasInEinemThread2()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Thread.Sleep(50);
+                Console.Write("#");
+            }
         }
     }
 }
