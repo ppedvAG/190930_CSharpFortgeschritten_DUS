854733d8c91c8545c20ca91b2349c7f5a70e5184
Michael Zöhling <michael.zoehling@outlook.com>
2019-10-02 14:50:21 +0200

    Operatorüberladung

---
+++ Operatorüberladung/Operatorüberladung/Bruch.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operatorüberladung
+{
+    class Bruch
+    {
+        public Bruch(int zähler, int nenner)
+        {
+            Zähler = zähler;
+            Nenner = nenner;
+        }
+
+        public int Zähler { get; set; }
+        public int Nenner { get; set; }
+
+
+        // Operatorüberladung:
+        // Arithm:
+        // +,-,*,/,%
+        // Binär:
+        // &,|,^,<<,>>
+        // Vergleichs:
+        // ==, !=, <,>,<=,>=
+        // ----> Paarweise (wenn ==, dann muss auch !=)
+
+        // Cast (implizit)
+        public static Bruch operator *(Bruch left, Bruch right)
+        {
+            return new Bruch(left.Zähler * right.Zähler, left.Nenner * right.Nenner);
+        }
+        public static Bruch operator *(Bruch left,int right)
+        {
+            return new Bruch(left.Zähler * right, left.Nenner);
+        }
+
+        public static bool operator ==(Bruch left, Bruch right)
+        {
+            return true;
+        }
+        public static bool operator !=(Bruch left, Bruch right)
+        {
+            return false;
+        }
+    }
+}
---
+++ Operatorüberladung/Operatorüberladung/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operatorüberladung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bruch b1 = new Bruch(1, 2);
+            Bruch b2 = new Bruch(1, 4);
+            Bruch b3 = new Bruch(1, 8);
+
+
+            Bruch erg = b1 * (b2 * b3);
+
+            Console.WriteLine($"{erg.Zähler}/{erg.Nenner}");
+
+            erg = b2 * 3;
+            Console.WriteLine($"{erg.Zähler}/{erg.Nenner}");
+
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
+        }
+    }
+}
