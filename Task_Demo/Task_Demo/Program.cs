e675b527dd2c18522c9ad3ee5e7adf4e01696a1e
Michael Zöhling <michael.zoehling@outlook.com>
2019-10-02 11:25:37 +0200

    Init Task_Demo

---
+++ Task_Demo/Task_Demo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            Console.WriteLine("---ANFANG---");
+            Console.ReadKey();
+        }
+    }
+}
